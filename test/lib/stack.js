39ec707e76b1d7ce8562ec90285fa40ac80e0218
Philip Hayes <phayes@PHAYES.local>
2016-07-19 15:20:43 -0700

    Added unit tests

---
+++ test/lib/stack.js
@@ -0,0 +1,65 @@
+var proxyquire = require('proxyquire');
+var mockRequest = function(options, cb){
+	var successResponse = {
+		"statusCode":202,
+		"body":{
+			"job": 17
+		},
+		"headers":{
+			"date":"Tue, 19 Jul 2016 16:14:58 GMT",
+			"server":"Apache/2.4.6 (Red Hat Enterprise Linux) OpenSSL/1.0.1e-fips mod_wsgi/3.4 Python/2.7.5",
+			"vary":"Accept,Cookie",
+			"allow":"GET, POST, HEAD, OPTIONS",
+			"x-api-time":"0.114s",
+			"connection":"close",
+			"transfer-encoding":"chunked",
+			"content-type":"application/json"
+		},
+		"request":{
+			"uri":{
+				"protocol":"https:",
+				"slashes":true,
+				"auth":"admin:admin",
+				"host":"tower-1.innovation.labs.redhat.com",
+				"port":443,
+				"hostname":"tower-1.innovation.labs.redhat.com",
+				"hash":null,
+				"search":null,
+				"query":null,
+				"pathname":"/api/v1/job_templates/7/launch/",
+				"path":"/api/v1/job_templates/7/launch/",
+				"href":"https://admin:admin@tower-1.innovation.labs.redhat.com/api/v1/job_templates/7/launch/"
+			},
+			"method":"POST",
+			"headers":{
+				"authorization":"Basic YWRtaW46YWRtaW4=",
+				"content-length":0
+			}
+		}
+	};
+	cb(null, successResponse, successResponse.body);
+}
+   
+describe('processStack calls', function () {
+  it('should return success', function (done) {
+  	var stack = proxyquire('../../lib/stack.js', {'request': mockRequest});
+  	stack.processStack( {projectName: 'testProject', getUrl:"http://127.0.0.1/"},function(err, response){
+		response.should.equal('Successful Launch with Ansible tower job ID: 17');
+		done();
+  	});
+  });
+  it('should return no extra_vars specified', function (done) {
+  	var stack = proxyquire('../../lib/stack.js', {'request': mockRequest});
+  	stack.processStack( {projectName: 'testProject'},function(err, response){
+		err.should.equal('no extra_vars specified');
+		done();
+  	});
+  });
+  it('should return error no extra_vars specified', function (done) {
+  	var stack = proxyquire('../../lib/stack.js', {'request': mockRequest});
+  	stack.processStack( {},function(err, response){
+		err.should.equal('no extra_vars specified');
+		done();
+  	});
+  });
+});
