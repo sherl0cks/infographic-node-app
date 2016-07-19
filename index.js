53a9092dfb2f9969497add8d0f1d4a7df7dc3aec
Matyas Danter <mdanter@gmail.com>
2016-07-19 15:00:33 -0700

    tower changes

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ app.use(cors());
 app.use(bodyParser());
 app.post('/stack', function (req, res) {
 	console.log(req.body);
-	console.log('in / route');
+	console.log('in /stack route');
 	stack.processStack(req.body,function(err, response){
 		if (err){
 			res.status(500).send(err);
 		} else {
 			res.send(response);
 		}
-		
-	});	
-	
-  	
+
+	});
+
+
 });
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
